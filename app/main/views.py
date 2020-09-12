038f9b1030f7a3d69b866014e665e4fa2625b1d7
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-13 01:19:01 +0300

    link the index.html to the articles.html by allowing a person to obtain news articles of the source by clicking on the source_title

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
 from ..request import get_sources
 from ..request import get_articles
 
+
 #views
-@app.route('/')
+@app.route('/' )
 def index():
     
     '''
