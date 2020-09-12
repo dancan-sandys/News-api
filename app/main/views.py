968f4b656d82e63dde9de08085986cdab7cabad2
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-12 23:51:08 +0300

    add a route to show the articles of a particular source

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from ..request import get_sources
+from ..request import get_articles
 
 #views
 @app.route('/')
@@ -12,3 +13,14 @@ def index():
     news_sources = get_sources()
     
     return render_template('index.html', news_sources = news_sources)
+
+@app.route('/articles/<source_id>')
+def articles(source_id):
+
+    '''
+    articles root that display the articles.html template
+    '''
+    
+    news_articles = get_articles(source_id)
+    
+    return f'hello {source_id}'
